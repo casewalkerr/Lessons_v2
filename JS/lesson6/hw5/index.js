ec0018eb41d485b3b7b1eaf882882bbedfdb1bf2
Roman_Abakumov <case777walker@gmail.com>
2020-05-07 20:52:11 +0300

    1

--- JS/lesson6/hw5/index.js
+++ JS/lesson6/hw5/index.js
@@ -1,7 +1,19 @@
+// function reverseArray(arr){
+//     if(!Array.isArray(arr)){
+//         return null;
+//     }
+//     return arr.reverse();
+// }
+// console.log(reverseArray([34,4,222,5,34,56]));
+
 function reverseArray(arr){
     if(!Array.isArray(arr)){
         return null;
     }
-    return arr.reverse();
+    reverseA = [];
+   for(let i =arr.length-1; i>0; i--){
+       reverseA.push(arr[i]);
+   }
+    return reverseA;
 }
 console.log(reverseArray([34,4,222,5,34,56]));

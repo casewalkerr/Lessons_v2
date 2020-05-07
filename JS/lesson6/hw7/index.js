5de5c43b65a7e81d31cba564b1cbceac35a2f28c
Roman_Abakumov <case777walker@gmail.com>
2020-05-07 23:02:29 +0300

    1

--- JS/lesson6/hw7/index.js
+++ JS/lesson6/hw7/index.js
@@ -4,7 +4,7 @@ function checher(arr){
     }
     let minValue = Math.min.apply(null, arr);
     let maxValue = Math.max.apply(null, arr);
-    let sumValue = (minValue + maxValue);
+    let sumValue = minValue + maxValue;
     return (sumValue > 1000);
 }
 console.log(checher([34,2113,23,1]));

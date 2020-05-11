4b9fbd61a509178e25601007fcef057b6800d241
Roman_Abakumov <case777walker@gmail.com>
2020-05-12 00:59:05 +0300

    1

--- JS/lesson7/task1/index.js
+++ JS/lesson7/task1/index.js
@@ -1,3 +1,22 @@
 const getSpecialNumbers = array => array.filter(num => num % 3 === 0);
 
 console.log(getSpecialNumbers([3, 5, 6, 4, 14, 56]));
+
+// const getSpecialNumbers = numbers => {
+//     let specialNumbers = [];
+//     function checkIfSpecialNumber(num) {
+//         if (num % 3 ===0 ) {
+//             specialNumbers.push(num);
+//         }
+//     }
+// //     return specialNumbers;
+// // }
+// const getSpecialNumbers = numbers => {
+//         let specialNumbers = [];
+//        numbers.forEach(num => {
+//            if (num % 3 ===0 ){
+//            specialNumbers.push(num);
+//         }
+//         });
+//         return specialNumbers;
+//     };

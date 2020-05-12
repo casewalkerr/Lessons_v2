cd842c1ab4d55ba136bfd70f7c22d24c2d8b7c3b
Roman_Abakumov <case777walker@gmail.com>
2020-05-12 15:34:59 +0300

    1

---
+++ JS/lesson7/hw3/index.js
@@ -0,0 +1,10 @@
+const increaseEvenEI =(arr, delta) => {
+    if(!Array.isArray(arr))
+    return null;
+    return arr.map(num => {
+        if(num % 2 === 0)
+        return num + delta;
+        return num;
+    })
+}
+console.log(increaseEvenEI([4,5,2,3,5], 12));

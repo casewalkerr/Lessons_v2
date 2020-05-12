0e6e4face46a41df8e1cf8ba51cad02b31f363fd
Roman_Abakumov <case777walker@gmail.com>
2020-05-12 15:19:43 +0300

    1

--- JS/lesson7/hw2/index.js
+++ JS/lesson7/hw2/index.js
@@ -1,6 +1,7 @@
 const reverseArray = array => {
     if(!Array.isArray(array)) 
     return null;
-    return array.slice().reverse();
+    const reverA = array.slice();
+    return reverA.reverse();
 }
 console.log(reverseArray([2,5,1,65,34,24]));

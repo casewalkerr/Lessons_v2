d44db518d0191021ace0ca94bd30e8048ff3ca1f
Roman_Abakumov <case777walker@gmail.com>
2020-05-12 14:40:47 +0300

    1

---
+++ JS/lesson7/task4/index.js
@@ -0,0 +1,7 @@
+const getMessagesForBestStudents = (allStudents, faildStudents) =>{
+const passedStudents = allStudents
+.filter(name => !faildStudents.includes(name));
+const masseges = passedStudents
+.map(name => 'Good job,' + name);
+return masseges;
+};

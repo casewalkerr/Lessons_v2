5d0343b21e80241b60389e58dee47a2815b759bb
Roman_Abakumov <case777walker@gmail.com>
2020-05-13 11:07:15 +0300

    1

--- JS/loops/task2/index.js
+++ JS/loops/task2/index.js
@@ -3,12 +3,12 @@ let n = 40;
 let result = 0;
 
 for(let i = m; i <= n; i++){
-if(i % 5 ===0){
+if(i % 5 === 0){
 // console.log( "4 if is called for number " + i)
-console.log(i);
-continue;
+ console.log(i);
+ continue;
 }
-if(i % 2 === 0 && i % 4 !==0){ 
+if(i % 2 === 0 && i % 4 !== 0){ 
 // console.log( "1 if is called for number " + i)
 result += i;
 continue;
@@ -24,4 +24,4 @@ if(i % 4 === 0){
     continue;
 }
 }
-console.log(result);
+// console.log(result);

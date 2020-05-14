28a40499660dee1f45a42a80679d09010097c93c
Roman_Abakumov <case777walker@gmail.com>
2020-05-14 23:05:12 +0300

    1

---
+++ JS/lesson9/hw1/index.js
@@ -0,0 +1,17 @@
+// input: obj;
+// output: arr;
+const customers = {
+    'customer-id-1':{
+        name: 'William',
+        age: 54,
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17,
+    },
+};
+const getCustomersList = obj => 
+    Object.entries(obj).map(([id, value])=> ({id,...value}))
+    .sort((a, b) => a.age -b.age);
+    
+    console.log(getCustomersList(customers));

3c2ec29da043638e87428dfc734365f1a3a08bab
Roman_Abakumov <case777walker@gmail.com>
2020-05-15 00:25:53 +0300

    1

---
+++ JS/lesson9/hw2/index.js
@@ -0,0 +1,23 @@
+// input: obj;
+// output: arr;
+
+rooms = {
+    room1:[
+        {name: 'room1 name1'},
+        {name: 'room1 name2'},
+        {name: 'room1 name3'},
+        {name: 'room1 name4'},
+    ],
+    room2:[
+        {name: 'room2 name1'}, 
+    ],
+    room3:[
+        {name: 'room3 name1'},
+        {name: 'room3 name2'},
+        {name: 'room3 name3'},
+    ],
+};
+const getPeople = obj => 
+    Object.values(obj).flat().map(obj => obj.name)
+
+console.log(getPeople(rooms));

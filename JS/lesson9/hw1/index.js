2aa7adf37c8f0bab31ea7c787b62c904664f73f3
Roman_Abakumov <case777walker@gmail.com>
2020-05-15 10:55:08 +0300

    1

--- JS/lesson9/hw1/index.js
+++ JS/lesson9/hw1/index.js
@@ -1,5 +1,8 @@
 // input: obj;
 // output: arr;
+// 1 обьект преобразовать в массивы массивов
+// 2 добавить id 
+// 3 отсортировать по возрасту
 const customers = {
     'customer-id-1':{
         name: 'William',
@@ -10,8 +13,8 @@ const customers = {
         age: 17,
     },
 };
-const getPeople = obj => 
-    Object.entries(obj).map(([id, value])=> ({id,...value}))
-    .sort((a, b) => a.age -b.age);
+const getCustomersList = obj => 
+    Object.entries(obj).map(([id, value]) => ({id, ...value})).
+    sort((a,b) => a.age -b.age);
     
     console.log(getCustomersList(customers));
---
+++ JS/standup/functions/15.05.20/task1/index.js
@@ -0,0 +1,37 @@
+// input: Number;
+// output:undefined;
+// 1. learn requirement & create algo
+// 2. create prototype solution
+// 3. refactoring & tisting
+// 4. final solution
+
+// algo
+// 1. iterate from 1 to n
+// 2. write func that checks number is prime
+// 3. of num is prime  print it
+
+// input: Number;
+// output:boolean;
+// algo
+// 1. iterate from 1 to num
+// 2. if num % i !==0 -false
+// 3. 
+function isPrime(num){
+    for(let i = 2; i < num; i++){
+        // console.log("check for: " + i);
+        if(num % i ===0){
+            // console.log("if is called for: " + i);
+            return false;
+        }
+    }
+    // console.log("loop is finished: " + i);
+ return true;
+}
+
+function getPrimes (n) {
+    for (let i=1; i<=n; i++){
+        if(isPrime(i)){
+            console.log(i);
+        }
+    }
+}

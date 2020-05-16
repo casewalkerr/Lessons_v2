badee2c3785bcca4758d73e56fb3d69c8bbc3d42
Roman_Abakumov <case777walker@gmail.com>
2020-05-16 23:13:14 +0300

    1

--- JS/lesson10/task4/index.js
+++ JS/lesson10/task4/index.js
@@ -3,7 +3,7 @@
 // 1 проверить массив на массив
 // 2 преобразовать все числа по модулю пройдясь по массиву 
 // 3  найти максимальное число
-const getMaxAbsolulteNumber = arr => {
+const getMaxAbsoluteNumber = arr => {
     if(!Array.isArray(arr)) return null;
     const absoluteValues = arr
     .map(num => Math.abs(num));

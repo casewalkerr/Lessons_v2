0afd9f3fc741fe0f3a7755b894861fd201b73229
Roman_Abakumov <case777walker@gmail.com>
2020-05-17 04:56:06 +0300

    1

--- JS/lesson10/hw3/index.js
+++ JS/lesson10/hw3/index.js
@@ -2,11 +2,11 @@
 // output: arr;
 // 1 округлить числа до numCount 
 // 2 вернуть массив 
-const superRound = (num, numCount) =>  [
-    Math.floor(num * numCount) / numCount,
-    Math.round(num * numCount) / numCount,
-    Math.ceil(num * numCount) / numCount,
-    Math.trunc(num * numCount) / numCount,
-    +num.toFixed(numCount)
+const superRound = (num, n) =>  [
+    Math.floor(num * n) / n,
+    Math.round(num * n) / n,
+    Math.ceil(num * n) / n,
+    Math.trunc(num * n) / n,
+    +num.toFixed(n)
 ]
 console.log(superRound(2.4555, 2));

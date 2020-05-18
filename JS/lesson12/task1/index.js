870982b2d92b1d258934f8c730afaab8f16c157d
Roman_Abakumov <case777walker@gmail.com>
2020-05-18 15:12:22 +0300

    1

---
+++ JS/lesson12/task1/index.js
@@ -0,0 +1,22 @@
+// input: str;
+// output: str;
+// 1 '1 + 2' => '1 + 2 = 3'
+const calc = expresion => {
+    const [a, operator, b] = expresion.split(' ');
+    let result;
+    switch (operator){
+        case '+':
+            result =  Number(a) + Number(b);
+            break;
+         case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+    }
+    return `${expresion} + ' = ' + ${result}`;
+}

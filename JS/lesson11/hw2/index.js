7c374302639f6673c78f4955d3bdaed09a745276
Roman_Abakumov <case777walker@gmail.com>
2020-05-18 14:28:04 +0300

    1

--- JS/lesson11/hw2/index.js
+++ JS/lesson11/hw2/index.js
@@ -2,7 +2,7 @@
 // output: [str];
 // 1 вернуть null если 2 arg не строка
 // 2 число вхождение 2 arg  в 1
-const countOccurences = (str,substr) => {
+const countOccurrences = (str,substr) => {
     if(substr == '') return null;
     return str.split(substr).length -1;
 }

3eac0081fa30b3ab329b08f43686abec0e077ab3
Roman_Abakumov <case777walker@gmail.com>
2020-05-20 02:03:36 +0300

    1

--- JS/lesson14/task2/index.js
+++ JS/lesson14/task2/index.js
@@ -2,12 +2,12 @@
 
 
   const createMessenger = () => {
-    let message = 'Just learn it';
+    let message = 'Just learn it!';
     let sender = 'Gromcode';
 
     function sendMessage(name){
         
-        console.log(`${name}, ${message}! Your ${sender}`);
+        console.log(`${name}, ${message} Your ${sender}`);
     }
     function setMessage(text) {
         message = text;
@@ -28,7 +28,7 @@ const messenger1 = createMessenger();
 const messenger2 = createMessenger();
 const messenger3 = createMessenger();
 messenger1.sendMessage('Bob');
-messenger2.setMessage('Good job');   //метод использующий сообщение 
+messenger2.setMessage('Good job!');   //метод использующий сообщение 
 messenger2.sendMessage('Bob');
 messenger3.setSender('Tom');
 messenger3.sendMessage('Bob');

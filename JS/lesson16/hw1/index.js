b39ff006c83478eac41c4ad5973ef3d085ce27ad
Roman_Abakumov <case777walker@gmail.com>
2020-05-21 22:40:05 +0300

    1

---
+++ JS/lesson16/hw1/index.js
@@ -0,0 +1,14 @@
+
+
+const createArreyOfFunction = num => {
+    let arr = [];
+    if( num === undefined) return [];
+    if(typeof num !== 'number') return null;
+    for(let i = 0; i < num; i ++) {
+        arr[i] = function(){
+            return i;
+        }
+    }
+    return arr;
+}
+console.log(createArreyOfFunction(96)[79]());

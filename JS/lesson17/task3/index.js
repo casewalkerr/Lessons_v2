15a010843ea719a5e93f4e0e4bcafc8949f1d040
Roman_Abakumov <case777walker@gmail.com>
2020-05-25 01:45:56 +0300

    1

---
+++ JS/lesson17/task3/index.js
@@ -0,0 +1,14 @@
+function defer(func,ms) {
+    return function() {
+        setTimeout(() => func.call(this, ...arguments), ms);
+    }
+}
+const user = {
+    name: 'Tom',
+    SayHi() {
+        console.log(`Hi, I'm ${this.name}`)
+    }
+}
+
+const defferredHi = defer(user.SayHi.bind(user), 2000)
+defferredHi();

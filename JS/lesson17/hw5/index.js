78a8d0755675824c6ec143051c67e3a7595c1ada
Roman_Abakumov <case777walker@gmail.com>
2020-05-25 16:49:56 +0300

    timer

---
+++ JS/lesson17/hw5/index.js
@@ -0,0 +1,25 @@
+
+const timer = {
+    secondsPassed: 0,
+    minsPassed: 0,
+    countTimer: 0,
+    startTimer() {
+        this.countTimer = setInterval(()=>{
+            if(this.secondsPassed == 60){
+                this.minsPassed++
+                this.secondsPassed = 0;
+            }
+        },1000)
+    },
+    getTime() {
+        return ` ${this.secondsPassed}:${this.minsPassed < 10 ?
+        '0' + this.minsPassed: this.minsPassed}`
+    },
+    stopTimer() {
+        clearInterval(this.countTimer);
+    },
+    resetTimer() {
+        this.secondsPassed = 0;
+        this.minsPassed = 0;
+    }
+}

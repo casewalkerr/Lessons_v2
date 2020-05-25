07fb9c8123909dcb27b0d4a24c8a9ba51dbc76f8
Roman_Abakumov <case777walker@gmail.com>
2020-05-25 15:37:00 +0300

    1

---
+++ JS/lesson17/hw4/index.js
@@ -0,0 +1,16 @@
+
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    },
+    setFullName(fN){
+        let res = fN.split(' ');
+        this.firstName = res[0];
+        this.lastName = res[1];
+        console.log(res[0])
+        console.log(res[1])
+    }
+}
+const g = user.setFullName('John Doe');

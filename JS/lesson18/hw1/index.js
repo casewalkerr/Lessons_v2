5f01720979ecdac43dfbf70a92ef9e0da9664181
Roman_Abakumov <case777walker@gmail.com>
2020-05-26 18:56:22 +0300

    1

--- JS/lesson18/hw1/index.js
+++ JS/lesson18/hw1/index.js
@@ -1,76 +1,38 @@
-/* ===> 1 <=== */
-const student = {
-    name: 'Tom',
-};
+'use strict';
 
- export function sayName() {
-    console.log(this.name);
-}
-
-// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента 
- sayName.call(student);
-
-
-// вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
-// ... your code here
-sayName.call({name: 'Bruce'});
-
-
-/* ===> 2 <=== */
-const company = {
-    companyName: 'Microsoft'
-};
-
-function greeting(firstName, lastName) {
-    console.log(`Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`);
-}
-
-// вызовите ф-цию greeting так, чтобы в консоль вывелось 
-// 'Hello, Bob Marley. Welcome to the Microsoft'
-// используйте объект company
-// ... your code here
-greeting.call({...company}, 'Bob','Marley')
-
-/* ===> 3 <=== */
-const country = {
-    countryName: 'Ukraine',
-    capital: 'Kyiv'
-};
-
-function getPopulation(population) {
-    return `Population in ${this.countryName} is ${population}`;
-}
-
-// вызовите ф-цию getPopulation так, чтобы она вернула 
-// 'Population in Ukraine is 43000'
-// 43000 передавайте в виде числа
-// используйте объект country
-// результат работы ф-ции getPopulation присвойте в переменную и выведите в консоль
-// ... your code here
-const populationOf = getPopulation.call(country, 43000);
-console.log(populationOf);
-
-
-/* ===> 4 <=== */
-const transaction = {
-    amount: 1200,
-    operation: 'sell',
-    currency: 'USD',
-    exchange: 'NYSE',
-    printTransaction() {
-        console.log(`${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`);
+// input: function
+// output: function witch saves all calls and contecst 
+function saveCalles (func) {
+    withMemory.calls = [];
+    function withMemory (...args){
+        withMemory.calls.push([...args]);
+        return func.call(this, args);
     }
+    return withMemory;
 }
 
-const anotherTransaction = {
-    amount: 400,
-    operation: 'buy',
-    currency: 'USD',
-    exchange: 'NASDAQ',
-};
-transaction.printTransaction.call(anotherTransaction);
 
-// вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
-// '400 USD - buy on NASDAQ'
-// используйте объект anotherTransaction как контекст
-// ... your code here
+function test (a,b) {
+    return Math.sqrt(a * a + b * b);
+}
+
+const testWithMemory = saveCalles(test);
+testWithMemory(4, 2);
+testWithMemory(9, 1);
+
+
+console.log(testWithMemory.calls) //[[4, 2], [9, 1]]
+/* Поддержка функции с контекстом */
+
+const user = {
+    name: 'john',
+    sayHi(){
+        return this.name
+    }
+};
+
+const methodWithMemory = saveCalles(user.sayHi);
+
+console.log(methodWithMemory.apply({name:'Tom'})); // 'Tom'
+
+console.log(methodWithMemory.calls); // [[]]

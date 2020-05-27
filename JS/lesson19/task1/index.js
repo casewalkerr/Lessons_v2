da979ac6559318ce338e8eff8805464c82efe5ca
Roman_Abakumov <case777walker@gmail.com>
2020-05-27 03:17:31 +0300

    1

--- JS/lesson19/task1/index.js
+++ JS/lesson19/task1/index.js
@@ -13,9 +13,11 @@ const vehicle = {
 const ship = {
    startMachine() {
        console.log(`${this.name} lifting anchor up`)
+       this.move();
    },
    stopMachine() {
        console.log(`${this.name} lifting anchor down`)
+       this.stop();
    },
     __proto__: vehicle
 }

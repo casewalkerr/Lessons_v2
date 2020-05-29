0419c88364ff28d6601b4410d9464309fab982a3
Roman_Abakumov <case777walker@gmail.com>
2020-05-30 01:31:52 +0300

    1

--- JS/lesson20/hw2/index.js
+++ JS/lesson20/hw2/index.js
@@ -26,6 +26,9 @@ class UserRepository {
     constructor(users) {
         this._users = Object.freeze(users);
     }
+    get users() {
+        return this._users;
+    }
     getUserNames() {
         return this._users.map(el => el.name)
     }

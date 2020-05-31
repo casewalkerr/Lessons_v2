58b15ece98d6b89acf5a2717403725b097db064f
Roman_Abakumov <case777walker@gmail.com>
2020-06-01 00:19:57 +0300

    1

---
+++ JS/lesson21/hw3/index.js
@@ -0,0 +1,27 @@
+
+
+const getTitle = () => {
+    const getTitle = document.querySelector('.title')
+    console.log(getTitle.textContent);
+    return getTitle.textContent;
+}
+
+const getDescription = () => {
+    const getDesc = document.querySelector('.about');
+    console.log(getDesc.innerText);
+    return getDesc;
+}
+const getPlans = () => {
+    const getPlan = document.querySelector('.plans');
+    console.log(getPlan.innerHTML);
+    return getPlan.innerHTML;
+}
+const getGoal = () => {
+    const getG = document.querySelector('.goal');
+    console.log(getG.outerHTML);
+    return getG.outerHTML;
+}
+getTitle();
+getDescription();
+getPlans();
+getGoal();

8df54ca42edd854bba881dbf74625f4a14e1136d
Roman_Abakumov <case777walker@gmail.com>
2020-05-31 23:39:26 +0300

    1

---
+++ JS/lesson21/hw2/index.js
@@ -0,0 +1,15 @@
+
+
+const getItemsList = () => {
+    const elementsList = document.querySelectorAll('.technology')
+    console.dir(elementsList);
+    return elementsList;
+}
+
+const getItemsArray = () => {
+    const NodeList = Array.from(document.querySelectorAll('.tool'));
+    console.dir(NodeList);
+    return NodeList;
+}
+getItemsList();
+getItemsArray();

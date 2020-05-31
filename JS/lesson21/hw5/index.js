18a9a9d78ad00a61a985758cdf524d38a0e35369
Roman_Abakumov <case777walker@gmail.com>
2020-06-01 01:04:24 +0300

    1

---
+++ JS/lesson21/hw5/index.js
@@ -0,0 +1,9 @@
+
+
+const setButton = buttonText => {
+    // const setB = document.querySelector('body')
+    // return setB.innerHTML = '<button>button text</button>'
+    return document.querySelector('body')
+        .innerHTML = `<button>${buttonText}</button>`
+}
+setButton('ggwp')

727085dbe77cac0b8859041ca13f0baa6d24cfee
Roman_Abakumov <case777walker@gmail.com>
2020-06-01 19:28:04 +0300

    1

---
+++ JS/lesson21/hw12/index.js
@@ -0,0 +1,53 @@
+
+
+// const getSection = num => {
+//     const getN = document.querySelector(`span[data-number="${num}"]`)
+//     return getN.closest('div').dataset.section
+// }
+
+export const getSection = num => {
+    return document.querySelector(`span[data-number="${num}"]`)
+        .closest('div').dataset.section
+}
+
+
+
+
+
+console.log(getSection(3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

a407deb64cb33eea4fbe005f7cc46cc7abd6694c
Roman_Abakumov <case777walker@gmail.com>
2020-06-01 10:50:42 +0300

    1

---
+++ JS/standup/object/task4/index.js
@@ -0,0 +1,38 @@
+
+//  algo
+//  1 convert the object to an array of arrays
+//  2 map entries to the array of object
+//  3 sort the array and return
+//  input object
+//  output Array
+function getCustomerList(obj) {
+    return Object.entries(obj)
+        .map(([id, object]) => ({ ...object, id }))
+        .sort((a, b) => a.age - b.age);
+
+    // console.log(entries);
+
+    // entries.map(entry => {
+    //     let id = entry[0];
+    //     let object = entry[1];
+    //     console.log(id);
+    //     console.log(object);
+    //     let res = { ...object, id };
+    //     // let res = { ...object, [id]: id };
+    //     console.log(res)
+    //     return res;
+    // })
+}
+
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17
+    },
+};
+
+console.log(getCustomerList(customers));
---
+++ JS/standup/object/task5/index.js
@@ -0,0 +1,32 @@
+
+// input:  arrays
+// output: array of objects
+
+// algo:
+//1. setting property isAdmin for every user
+//2. compare users id to adminids array elements
+//2. if they coincide, add to user object property isAdmin:true
+//3. if there are no coincides, thought isAdmin: false.
+
+function markAdmins(users, adminIds) {
+    return users.map(user => {
+        let isAdmin = adminIds.includes(user.id);
+        //  user.isAdmin = isAdmin;
+        return res = { ...user, isAdmin };;
+    });
+
+}
+
+
+const markAdmins1 = (users, adminIds) =>
+    users.map(user => ({ ...user, isAdmin: adminIds.includes(user.id) }));
+
+
+const adminIds = ['1', '3'];
+const users = [
+    { id: '1', name: 'Bob' },
+    { id: '2', name: 'Tom' },
+    { id: '3', name: 'Sam' },
+    { id: '4', name: 'Tad' }
+];
+console.log(markAdmins(users, adminIds))

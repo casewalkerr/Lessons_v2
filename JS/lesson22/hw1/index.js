92fe464428300c7e3bc0e362c0ac81d658426caa
Roman_Abakumov <case777walker@gmail.com>
2020-06-02 01:51:39 +0300

    1

---
+++ JS/lesson22/hw1/index.js
@@ -0,0 +1,3 @@
+const button = document.querySelector('.create-btn');
+const buttonfunc = () => { alert('done') }
+button.addEventListener('click', buttonfunc)
---
+++ JS/lesson22/task1/index.js
@@ -0,0 +1,6 @@
+
+
+const elem = document.querySelector('.rect_div');
+elem.addEventListener('click', () => {
+    console.log('div')
+})

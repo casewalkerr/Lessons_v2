104d4af0582136de8c8f016d8ea0a2401b4b3a18
Roman_Abakumov <case777walker@gmail.com>
2020-06-01 16:32:01 +0300

    1

--- JS/lesson21/hw11/index.js
+++ JS/lesson21/hw11/index.js
@@ -1,5 +1,5 @@
 
-const squaredNumbers = () => {
+export const squaredNumbers = () => {
     const numbers = document.querySelectorAll('.number');
     for (let el of numbers) {
         const sqr = el.dataset.number ** 2;

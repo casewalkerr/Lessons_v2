74a62352acb990fd75667d779483e9eb82d3bbfe
Roman_Abakumov <case777walker@gmail.com>
2020-06-01 15:05:49 +0300

    1

---
+++ JS/lesson21/hw10/index.js
@@ -0,0 +1,20 @@
+
+// const manageClasse = () => {
+//     const classOne = document.querySelector('.one').classList.add('selected');
+//     const classTwo = document.querySelector('.two').classList.remove('selected');
+//     const classThree = document.querySelector('.three').classList.toggle('three_done');
+//     const classFour = document.querySelector('.four')
+//     if (classFour.classList.contains('some-class')) {
+//         classFour.classList.add('another-class')
+//     }
+// }
+export const manageClasse = () => {
+    document.querySelector('.one').classList.add('selected');
+    document.querySelector('.two').classList.remove('selected');
+    document.querySelector('.three').classList.toggle('three_done');
+
+    if (document.querySelector('.four').classList.contains('some-class')) {
+        document.querySelector('.four').classList.add('another-class')
+    }
+}
+// manageClasse();

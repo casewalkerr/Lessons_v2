59ac3c2e12454b5662e027620777abac1f646140
Roman_Abakumov <case777walker@gmail.com>
2020-06-06 22:14:50 +0300

    1

--- JS/lesson23/task2/index.js
+++ JS/lesson23/task2/index.js
@@ -53,7 +53,7 @@ const onSeatSelect = event => {
     const sectoruNumber = event.target.closest('.sector').dataset.sectorNumber;
 
     const selectedSeatElem = document.querySelector('.board__selected-seat');
-    selectedSeatElem.textContent = `S ${seatNumber} - L ${lineNumber} - S${sectoruNumber}`
+    selectedSeatElem.textContent = `S ${seatNumber} - L ${lineNumber} - S ${sectoruNumber}`
 
 }
 arenaElem.addEventListener('click', onSeatSelect);

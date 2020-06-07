36a2680f0bde3da8c890c2026659c4c2b045f278
Roman_Abakumov <case777walker@gmail.com>
2020-06-08 01:44:19 +0300

    1

--- JS/lesson24/hw1/index.js
+++ JS/lesson24/hw1/index.js
@@ -5,12 +5,18 @@
 export const getDiff = (startDate, EndDate) => {
     const start = new Date(startDate);
     const end = new Date(EndDate);
-    const difDays = Math.floor(Math.abs(start - end) / (60 * 60 * 24 * 1000))
-    const difHours = Math.abs(start.getHours() - end.getHours())
-    const difMinutes = Math.abs(start.getMinutes() - end.getMinutes())
-    const difSeconds = Math.abs(start.getSeconds() - end.getSeconds())
+    const differ = Math.abs(start - end);
+    // const difDays = Math.abs(start - end) / (60 * 60 * 24 * 1000))
+    // const difHours = Math.abs(start.getHours() - end.getHours())
+    // const difMinutes = Math.abs(start.getMinutes() - end.getMinutes())
+    // const difSeconds = Math.abs(start.getSeconds() - end.getSeconds())
+    const difDays = Math.floor(differ / (1000 * 60 * 60 * 24))
+    const difHours = Math.floor(differ / (1000 * 60 * 60) % 24)
+    const difMinutes = Math.floor(differ / (1000 * 60) % 60)
+    const difSeconds = Math.floor(differ / 1000) % 60
     return `${difDays}d ${difHours}h ${difMinutes}m ${difSeconds}s`
 }
 // const startDate = new Date(2031, 08, 20, 15, 34, 50)
-// const EndDate = new Date(2019, 10, 07, 1, 34, 55);
+// const EndDate = new Date(2029, 10, 07, 1, 34, 55);
 // console.log(getDiff(startDate, EndDate))
+

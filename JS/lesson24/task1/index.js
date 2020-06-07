f409e0572ea92c65e16ecbff045a4240b749d8dc
Roman_Abakumov <case777walker@gmail.com>
2020-06-07 21:27:12 +0300

    1

--- JS/lesson24/task1/index.js
+++ JS/lesson24/task1/index.js
@@ -1,9 +1,9 @@
 export const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
-const getDayOfWeek = (date, days) => {
+const dayOfWeek = (date, days) => {
     const getDay = new Date(date).getDate();
     const futureInDate = new Date(date).setDate(getDay + days);
     return weekDays[new Date(futureInDate).getDay()];
 }
 
-const result = getDayOfWeek(new Date(2020, 06, 13), 3);
+const result = dayOfWeek(new Date(2020, 06, 13), 3);
 console.log(result)

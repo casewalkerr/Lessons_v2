f1c1e5ba5aabae925a685910864ce860304e53f1
Roman_Abakumov <case777walker@gmail.com>
2020-06-07 22:22:09 +0300

    1

--- JS/lesson24/task2/index.js
+++ JS/lesson24/task2/index.js
@@ -1,9 +1,9 @@
-export const formater = new Intl.DateTimeFormat('en', {
+const formater = new Intl.DateTimeFormat('en', {
     hour: '2-digit',
     minute: '2-digit',
     hour12: false,
     timeZone: 'UTC',
 })
-const getGreenwichTime = date => formater.format(date);
+export const getGreenwichTime = date => formater.format(date);
 
 // console.log(getGreenwichTime(new Date()))

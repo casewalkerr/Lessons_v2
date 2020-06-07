daf95076f8e9d4204d62cdaeff52f75ad453c5c2
Roman_Abakumov <case777walker@gmail.com>
2020-06-08 01:54:54 +0300

    1

--- JS/lesson24/hw1/index.js
+++ JS/lesson24/hw1/index.js
@@ -2,11 +2,11 @@
 // 2 находим разницу между датами в днях, часах, минутах, секундах
 // 3 возращаем в формате `23d 23h 34m 34s`
 
-export const getDiff = (startDate, EndDate) => {
+const getDiff = (startDate, EndDate) => {
     const start = new Date(startDate);
     const end = new Date(EndDate);
     const differ = Math.abs(start - end);
-    // const difDays = Math.abs(start - end) / (60 * 60 * 24 * 1000))
+    // const difDays = Math.floor(Math.abs((start - end) / (60 * 60 * 24 * 1000)))
     // const difHours = Math.abs(start.getHours() - end.getHours())
     // const difMinutes = Math.abs(start.getMinutes() - end.getMinutes())
     // const difSeconds = Math.abs(start.getSeconds() - end.getSeconds())
@@ -16,7 +16,9 @@ export const getDiff = (startDate, EndDate) => {
     const difSeconds = Math.floor(differ / 1000) % 60
     return `${difDays}d ${difHours}h ${difMinutes}m ${difSeconds}s`
 }
-// const startDate = new Date(2031, 08, 20, 15, 34, 50)
-// const EndDate = new Date(2029, 10, 07, 1, 34, 55);
-// console.log(getDiff(startDate, EndDate))
+const startDate = new Date(2032, 11, 20, 15, 34, 50)
+const EndDate = new Date(2031, 10, 20, 15, 34, 55);
+console.log(getDiff(startDate, EndDate))
 
+// 682d 12h 59m 55s
+// 29d 0h 0m 5s

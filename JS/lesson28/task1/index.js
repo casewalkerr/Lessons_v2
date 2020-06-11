87473e5dbbd8a239ba347c895851fceaac21454a
Roman_Abakumov <case777walker@gmail.com>
2020-06-11 16:43:02 +0300

    1

---
+++ JS/lesson28/task1/index.js
@@ -0,0 +1,28 @@
+// calc(3).add(2).mult(2).div(2).substract(4).result(); // 3
+
+const calc = initValue => {
+    let result = initValue;
+    const calculator = {
+        add(value) {
+            result += value;
+            return this;
+        },
+        mult(value) {
+            result *= value;
+            return this;
+        }, substract(value) {
+            result -= value;
+            return this;
+        },
+        div(value) {
+            result /= value;
+            return this;
+        },
+        result() {
+            return result
+        }
+    }
+    return calculator;
+}
+const res = calc(4).add(3).div(4).result()
+console.log(res)

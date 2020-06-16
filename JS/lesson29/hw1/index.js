bba99c713ab5e61e052e2348cc7934be667c29df
Roman_Abakumov <case777walker@gmail.com>
2020-06-16 16:15:00 +0300

    1

--- JS/lesson29/hw1/index.js
+++ JS/lesson29/hw1/index.js
@@ -8,7 +8,7 @@ export const requestUserData = (userId, callback) => {
     setTimeout(() => callback({
         userId,
         email: `${userId}@example.com`
-    }), Math.floor(Math.random() * 3000));
+    }), Math.floor(Math.random() * 3 * 1000));
 }
 const ddd = () => {
     console.log('Hi')

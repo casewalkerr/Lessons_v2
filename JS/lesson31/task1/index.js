c287f7162d800c2d6138aea0b95a3ffbabfcbbab
Roman_Abakumov <case777walker@gmail.com>
2020-06-17 16:32:57 +0300

    1

---
+++ JS/lesson31/task1/index.js
@@ -0,0 +1,22 @@
+export const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'));
+            }, 500);
+        } else
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    email: `${userId}@example.com`,
+                })
+            }, 1000)
+    })
+    return request;
+}
+
+// requestUserData('Roman')
+//     .catch(error => console.log(error))
+//     .then(data => console.log(data))
+    // .finaly(data => console.log(data))

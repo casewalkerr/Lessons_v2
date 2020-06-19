118a45ae7418368995590fa4e5b58e179c3e1531
Roman_Abakumov <case777walker@gmail.com>
2020-06-19 04:46:22 +0300

    1

--- JS/lesson32/task1/index.js
+++ JS/lesson32/task1/index.js
@@ -1,22 +1,26 @@
 // asyncSum(asyncNum1, asyncNum2, ...)
+
 const getValueWithDelay = (value, delay) => new Promise(resolve => {
     setTimeout(() => {
-        resolve(value)
-    }, delay)
-})
-const asyncNum1 = getValueWithDelay(45, 1000)
-const asyncNum2 = getValueWithDelay(3, 3000)
-const asyncNum3 = getValueWithDelay(34, 4000)
+        console.log(value);
+        resolve(value);
+    }, delay);
+});
+
+const asyncNum1 = getValueWithDelay(56, 1000);
+const asyncNum2 = getValueWithDelay('6', 2000);
+const asyncNum3 = getValueWithDelay(3, 3000);
 
 const getSum = numbers =>
     numbers
         .filter(value => !isNaN(value))
         .reduce((acc, num) => acc + Number(num), 0);
+
 const asyncSum = (...asyncNumbers) => {
-    return Promise.alld(asyncNumbers)
+    return Promise.all(asyncNumbers)
         .then(numbers => getSum(numbers))
-        .catch(() => Promise.reject(new Error('Can\'t calculate')))
-}
+        .catch(() => Promise.reject(new Error('Cant\'t calculate')));
+};
 
 asyncSum(asyncNum1, asyncNum2, asyncNum3)
     .then(result => console.log(result));

63f330321f5c936894ee3323d493a22aa1f8a3e0
Roman_Abakumov <case777walker@gmail.com>
2020-06-22 20:08:53 +0300

    1

--- JS/lesson33/hw1/index.js
+++ JS/lesson33/hw1/index.js
@@ -2,8 +2,9 @@ const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 /* getTasksList code here */
 
-export const getTaskList = () => fetch(baseUrl)
-    .then(response => response.json());
+export const getTasksList = () => fetch(baseUrl)
+    .then(response => response.json())
+
 /* getTaskById code here */
 export const getTaskById = (taskId) => fetch(`${baseUrl}/${taskId}`)
     .then(response => response.json())

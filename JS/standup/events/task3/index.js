368341b6adddb05f8baac4c47ad1e54c59dee775
Roman_Abakumov <case777walker@gmail.com>
2020-06-24 10:20:36 +0300

    1

--- JS/standup/events/task3/index.js
+++ JS/standup/events/task3/index.js
@@ -1,8 +1,10 @@
 
-const handleCk = () => {
+const handleCk = (event) => {
     if (event.target.classList.contains('list-item')) {
         console.log(event.target.textContent);
     }
 };
 document.querySelector('.List').addEventListener('click', handleCk)
+
+const listItemElem = document.querySelector('.list');
 
